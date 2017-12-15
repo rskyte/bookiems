5cc6cdb625271d9be2e7c5b52c49eca0a0edb563
Rhys Kyte <rhyskyte@Rhyss-MacBook-Pro.local>
2017-12-15 12:20:51 +0000

    password encryption added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,19 @@
 require './app/data_mapper_setup.rb'
+require 'bcrypt'
 
 class User
+  include BCrypt
   include DataMapper::Resource
 
   property :id,         Serial
   property :email,      String
-  property :password,   Text
+  property :password_digest,   Text
+
+
+
+  def password=(password)
+    #@password =
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
 end
